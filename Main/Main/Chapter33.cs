2af79d090bbf9fc7d637575ff297a0c68913bcea
CaigerDeng <416715486@qq.com>
2021-07-07 10:05:11 +0800

    update

--- Main/Main/Chapter33.cs
+++ Main/Main/Chapter33.cs
@@ -8,9 +8,36 @@ namespace Chapter33
     {
         static public void Run()
         {
-            
+            // https://leetcode-cn.com/problems/implement-strstr/
+
 
         }
+
+        // 暴力
+        public int BF(string haystack, string needle)
+        {
+            // 考虑haystack = needle，i + needle.Length <= haystack.Length 可以写等于号
+            for (int i = 0; i + needle.Length <= haystack.Length; i++)
+            {
+                bool flag = true;
+                for (var j = 0; j < needle.Length; j++)
+                {
+                    if (haystack[i + j] != needle[j])
+                    {
+                        flag = false;
+                        break;
+                    }
+                }
+                if (flag)
+                {
+                    return i;
+                }
+            }
+            return -1;
+
+        }
+
+
 
 
     }
